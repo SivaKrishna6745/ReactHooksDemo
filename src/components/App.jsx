fda03ecec3f56b62432af5239a361b668f6b88ba
SivaKrishna6745 <krishnasiva6745@gmail.com>
2021-12-04 18:27:32 +0530

    display time on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString("en-GB");
+  let [currentTime, setTime] = useState(time);
+
+  function getTime() {
+    let newTime = new Date().toLocaleTimeString("en-GB");
+    setTime(newTime);
+  }
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
